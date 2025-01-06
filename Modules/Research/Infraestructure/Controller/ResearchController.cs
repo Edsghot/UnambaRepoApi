e908bd6fae5a498c1c5f358bfd3f1cd755ee6100
edsghot <edsghot@gmail.com>
2025-01-05 22:06:32 -0500

    feat: pecanoSrc added

--- Modules/Research/Infraestructure/Controller/ResearchController.cs
+++ Modules/Research/Infraestructure/Controller/ResearchController.cs
@@ -34,68 +34,175 @@ public class ResearchController : ControllerBase
 
         return Ok(response);
     }
-    
+
     [HttpGet("pecanoSrc")]
     public IActionResult PecanoSrc()
     {
-
         var response = new
         {
             message = "ok",
             success = true,
-            data = new
+            data = new[]
             {
-                NomTipoDocumento = "Factura Electronica",
-                AbrevTipoDocumento = "FA",
-                SerieCompra = "F447",
-                NumCompra = "0145575",
-                DocumentoProveedor = "20554545743",
-                TipoDocumento = 2,
-                RazonSocial = "CORPORACION PRIMAX S.A.",
-                Sucursal = "Av Circunvalación del Club Golf Los Incas N° 134, Edificio Panorama Torre 1 Piso 18",
-                FechaEmision = "2024-10-24",
-                FechaVencimiento = "2024-11-11",
-                Moneda = "N",
-                Condicion = "R",
-                Observacion = "Compra de combustible",
-                Scop = "12427335754",
-                TotalGravadas = 97367.27m,
-                TotalExoneradas = 0.0m,
-                TotalOtrosTributos = 0.0m,
-                TotalPercepcion = 1148.93m,
-                TotalIGV = 17526.11m,
-                TotalPagar = 116042.31m,
-                Compras = new[]
+                // Comprobante correcto
+                new
                 {
-                    new
+                    NomTipoDocumento = "Factura Electronica",
+                    AbrevTipoDocumento = "FA",
+                    SerieCompra = "F447",
+                    NumCompra = "000145575",
+                    DocumentoProveedor = "20554545743",
+                    TipoDocumento = 2,
+                    RazonSocial = "CORPORACION PRIMAX S.A.",
+                    Sucursal = "Av Circunvalación del Club Golf Los Incas N° 134, Edificio Panorama Torre 1 Piso 18",
+                    FechaEmision = "2024-10-24",
+                    FechaVencimiento = "2024-11-11",
+                    Moneda = "N",
+                    Condicion = "R",
+                    Observacion = "Compra de combustible",
+                    Scop = "1242733575",
+                    TotalGravadas = 97367.27m,
+                    TotalExoneradas = 0.00m,
+                    TotalOtrosTributos = 0.00m,
+                    TotalPercepcion = 1148.93m,
+                    TotalIGV = 17526.11m,
+                    TotalPagar = 116042.31m,
+                    Compras = new[]
                     {
-                        codigo = "40002024",
-                        serie = "ABC123",
-                        Tieneserie = true,
-                        cantidad = 9100,
-                        descripcion = "Endura Super Diesel B5 S50",
-                        API = 36.90m,
-                        temp = 15.0m,
-                        precioUnitario = 97367.27m,
-                        Fise = 97367.27m,
-                        dscto = 0.0m,
-                        ISC = 0.0m,
-                        tieneIGV = 1,
-                        IGV = 17526.11m,
-                        Tratamiento = 10,
-                        subTotal = 97367.27m,
-                        total = 114893.38m
-                    }
+                        new
+                        {
+                            codigo = "40002024",
+                            serie = "ABC123",
+                            Tieneserie = true,
+                            cantidad = 9100,
+                            descripcion = "Endura Super Diesel B5 S50",
+                            API = 36.90m,
+                            temp = 15.0m,
+                            precioUnitario = 97367.27m,
+                            Fise = 97367.27m,
+                            dscto = 0.00m,
+                            ISC = 0.00m,
+                            tieneIGV = 1,
+                            IGV = 17526.11m,
+                            Tratamiento = 10,
+                            subTotal = 97367.27m,
+                            total = 114893.38m
+                        }
+                    },
+                    TipoDocReferencia = "Guía de Remisión",
+                    CorrelativoReferencia = "1-6306405",
+                    FechaEmisionReferencia = "2024-10-22",
+                    PlacaTransportista = "ARI-836",
+                    LicenciaTransportista = "H-43381852",
+                    MarcaTransportista = "HYUNDAI"
                 },
-                TipoDocReferencia = "Guía de Remisión",
-                CorrelativoReferencia = "1-6306405",
-                FechaEmisionReferencia = "2024-10-22",
-                PlacaTransportista = "ARI-836",
-                LicenciaTransportista = "H-43381852",
-                MarcaTransportista = "HYUNDAI"
+                // Comprobante con error: DocumentoProveedor demasiado largo
+                new
+                {
+                    NomTipoDocumento = "Factura Electronica",
+                    AbrevTipoDocumento = "FA",
+                    SerieCompra = "F447",
+                    NumCompra = "000145575",
+                    DocumentoProveedor = "205545457432055454574320554545743", // Error: demasiado largo
+                    TipoDocumento = 2,
+                    RazonSocial = "CORPORACION PRIMAX S.A.",
+                    Sucursal = "Av Circunvalación del Club Golf Los Incas N° 134, Edificio Panorama Torre 1 Piso 18",
+                    FechaEmision = "2024-10-24",
+                    FechaVencimiento = "2024-11-11",
+                    Moneda = "N",
+                    Condicion = "R",
+                    Observacion = "Compra de combustible",
+                    Scop = "1242733575",
+                    TotalGravadas = 97367.27m,
+                    TotalExoneradas = 0.00m,
+                    TotalOtrosTributos = 0.00m,
+                    TotalPercepcion = 1148.93m,
+                    TotalIGV = 17526.11m,
+                    TotalPagar = 116042.31m,
+                    Compras = new[]
+                    {
+                        new
+                        {
+                            codigo = "40002024",
+                            serie = "ABC123",
+                            Tieneserie = true,
+                            cantidad = 9100,
+                            descripcion = "Endura Super Diesel B5 S50",
+                            API = 36.90m,
+                            temp = 15.0m,
+                            precioUnitario = 97367.27m,
+                            Fise = 97367.27m,
+                            dscto = 0.00m,
+                            ISC = 0.00m,
+                            tieneIGV = 1,
+                            IGV = 17526.11m,
+                            Tratamiento = 10,
+                            subTotal = 97367.27m,
+                            total = 114893.38m
+                        }
+                    },
+                    TipoDocReferencia = "Guía de Remisión",
+                    CorrelativoReferencia = "1-6306405",
+                    FechaEmisionReferencia = "2024-10-22",
+                    PlacaTransportista = "ARI-836",
+                    LicenciaTransportista = "H-43381852",
+                    MarcaTransportista = "HYUNDAI"
+                },
+                // Comprobante con error: Scop con longitud incorrecta
+                new
+                {
+                    NomTipoDocumento = "Factura Electronica",
+                    AbrevTipoDocumento = "FA",
+                    SerieCompra = "F447",
+                    NumCompra = "000145575",
+                    DocumentoProveedor = "20554545743",
+                    TipoDocumento = 2,
+                    RazonSocial = "CORPORACION PRIMAX S.A.",
+                    Sucursal = "Av Circunvalación del Club Golf Los Incas N° 134, Edificio Panorama Torre 1 Piso 18",
+                    FechaEmision = "2024-10-24",
+                    FechaVencimiento = "2024-11-11",
+                    Moneda = "N",
+                    Condicion = "R",
+                    Observacion = "Compra de combustible",
+                    Scop = "12427335754", // Error: debe tener exactamente 10 caracteres
+                    TotalGravadas = 97367.27m,
+                    TotalExoneradas = 0.00m,
+                    TotalOtrosTributos = 0.00m,
+                    TotalPercepcion = 1148.93m,
+                    TotalIGV = 17526.11m,
+                    TotalPagar = 116042.31m,
+                    Compras = new[]
+                    {
+                        new
+                        {
+                            codigo = "40002024",
+                            serie = "ABC123",
+                            Tieneserie = true,
+                            cantidad = 9100,
+                            descripcion = "Endura Super Diesel B5 S50",
+                            API = 36.90m,
+                            temp = 15.0m,
+                            precioUnitario = 97367.27m,
+                            Fise = 97367.27m,
+                            dscto = 0.00m,
+                            ISC = 0.00m,
+                            tieneIGV = 1,
+                            IGV = 17526.11m,
+                            Tratamiento = 10,
+                            subTotal = 97367.27m,
+                            total = 114893.38m
+                        }
+                    },
+                    TipoDocReferencia = "Guía de Remisión",
+                    CorrelativoReferencia = "1-6306405",
+                    FechaEmisionReferencia = "2024-10-22",
+                    PlacaTransportista = "ARI-836",
+                    LicenciaTransportista = "H-43381852",
+                    MarcaTransportista = "HYUNDAI"
+                }
             }
         };
-            return Ok(response);
+        return Ok(response);
     }
 
 
