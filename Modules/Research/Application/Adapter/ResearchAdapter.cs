8311fc4b73326653e7f5ff197ff998be77af429e
edsghot <edsghot@gmail.com>
2025-02-02 15:42:13 -0500

    avance

--- Modules/Research/Application/Adapter/ResearchAdapter.cs
+++ Modules/Research/Application/Adapter/ResearchAdapter.cs
@@ -32,6 +32,7 @@ public class ResearchAdapter : IResearchInputPort
         _peruDateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, peruTimeZone);
         var account = new Account("dd0qlzyyk", "952839112726724", "7fxZGsz7Lz2vY5Ahp6spldgMTW4");
         _cloudinary = new Cloudinary(account);
+        
     }
 
     public async Task CreateResearchProjectAsync(CreateResearchProjectDto createDto)
@@ -216,19 +217,31 @@ public class ResearchAdapter : IResearchInputPort
         _researchOutPort.Success(articleDtos, "data");
     }
 
-
     private async Task<string> UploadPdf(IFormFile file, string folder)
     {
-        await using var stream = file.OpenReadStream();
-        var uploadParams = new RawUploadParams
+        try
         {
-            File = new FileDescription(file.FileName, stream),
-            Folder = folder
-        };
+            var account = new Account("dd0qlzyyk", "952839112726724", "7fxZGsz7Lz2vY5Ahp6spldgMTW4");
+            await using var stream = file.OpenReadStream();
+            var uploadParams = new RawUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = folder
+            };
 
-        var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
 
-        if (uploadResult.StatusCode == System.Net.HttpStatusCode.OK) return uploadResult.Url.AbsoluteUri;
-        return "";
+            if (uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return uploadResult.Url.AbsoluteUri;
+            }
+            return "";
+        }
+        catch (Exception ex)
+        {
+            // Manejo de la excepción, por ejemplo, registrar el error
+            Console.WriteLine($"Error al subir el archivo PDF: {ex.Message}");
+            return "";
+        }
     }
 }
